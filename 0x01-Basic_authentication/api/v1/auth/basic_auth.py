79473bff50c1dc79c4fa69cf9bf0e42eb3e3be76
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-05-03 00:17:59 +0200

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Basic Authentication class
 
 from api.v1.auth.auth import Auth
 import re
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -24,3 +26,18 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes a base64 authorization header
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
