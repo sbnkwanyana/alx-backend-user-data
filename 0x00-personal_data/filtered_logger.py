de30b4e24d9eab78d5a61a17751f19df03424bea
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-05-02 09:03:11 +0200

    two projects

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+"""
+
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    """
+    temp = message
+    for field in fields:
+        temp = re.sub(field + "=.*?" + separator,
+                      field + "=" + redaction + separator, temp)
+    return temp
+
+
+fields = ["password", "date_of_birth"]
+messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+for message in messages:
+    print(filter_datum(fields, 'xxx', message, ';'))
